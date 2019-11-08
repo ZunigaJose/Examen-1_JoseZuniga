d61af8ecabc1f569eca9ac35faf1803e4caea30d
Jose Zuniga <zunigajose21@gmail.com>
2019-11-08 13:30:17 -0600

    Creacion de clases libro y persona

---
+++ Examen1_JoseZuniga/src/examen1_josezuniga/Libro.java
@@ -0,0 +1,98 @@
+package examen1_josezuniga;
+
+import java.util.Date;
+
+public class Libro {
+    private String titulo, descripcion, genero, autor;
+    private Date year;
+    private int puntaje, copias, valor, edicion;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String descripcion, String genero, String autor, Date year, int puntaje, int copias, int valor, int edicion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.genero = genero;
+        this.autor = autor;
+        this.year = year;
+        this.puntaje = puntaje;
+        this.copias = copias;
+        this.valor = valor;
+        this.edicion = edicion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getYear() {
+        return year;
+    }
+
+    public void setYear(Date year) {
+        this.year = year;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public int getCopias() {
+        return copias;
+    }
+
+    public void setCopias(int copias) {
+        this.copias = copias;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(int edicion) {
+        this.edicion = edicion;
+    }
+    
+    
+}
---
+++ Examen1_JoseZuniga/src/examen1_josezuniga/Persona.java
@@ -0,0 +1,79 @@
+package examen1_josezuniga;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Persona {
+    private String nombreusuario, contraseña, correo, generoF;
+    private Date nacimiento;
+    private ArrayList<Libro> libros = new ArrayList<>();
+
+    public Persona() {
+    }
+
+    public Persona(String nombreusuario, String contraseña, String correo, String generoF, Date nacimiento) {
+        this.nombreusuario = nombreusuario;
+        this.contraseña = contraseña;
+        this.correo = correo;
+        this.generoF = generoF;
+        this.nacimiento = nacimiento;
+    }
+
+    public String getNombreusuario() {
+        return nombreusuario;
+    }
+
+    public void setNombreusuario(String nombreusuario) {
+        this.nombreusuario = nombreusuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getGeneroF() {
+        return generoF;
+    }
+
+    public void setGeneroF(String generoF) {
+        this.generoF = generoF;
+    }
+
+    public Date getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(Date nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public ArrayList<Libro> getLibros() {
+        return libros;
+    }
+
+    public void setLibros(ArrayList<Libro> libros) {
+        this.libros = libros;
+    }
+    
+    public Libro getLibro(int pos) {
+        return libros.get(pos);
+    }
+
+    public void setLibro(Libro libro) {
+        this.libros.add(libro);
+    }
+    
+    
+}
